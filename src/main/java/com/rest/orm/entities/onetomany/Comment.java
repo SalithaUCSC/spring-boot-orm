866a566f378181dc011091059f275387be2f209d
Salitha Chathuranga <salithachathuranga94@gmail.com>
2022-09-20 16:00:32 +0530

    remove unused imports

--- src/main/java/com/rest/orm/entities/onetomany/Comment.java
+++ src/main/java/com/rest/orm/entities/onetomany/Comment.java
@@ -1,12 +1,9 @@
 package com.rest.orm.entities.onetomany;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 
